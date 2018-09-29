1d72b33d306d1d049eb41defe3680770c501ce45
huangzj <huangzj@mzfdeMacBook-Pro.local>
2018-09-29 19:27:42 +0800

    sf

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,9 +1,18 @@
 export default {
     // Support type as Object and Array
-    'GET /api/users': { users: [1,2] },
+    'GET /api/users': {
+        users: [1, 2]
+    },
 
     // Method like GET or POST can be omitted
-    '/api/users/1': { id: 1 },
+    '/api/users/1': {
+        id: 1
+    },
+    '/api/user/:id': (req, res) => {
+        res.json({
+            id: req.params.id
+        })
+    },
 
     'POST /api/users': (req, res) => {
         const id = parseInt(req.query.id);
@@ -11,20 +20,26 @@ export default {
             case 1:
                 res.json({
                     success: true,
-                    data: {id: id},
+                    data: {
+                        id: id
+                    },
                 });
                 break;
             default:
                 res.json({
                     success: true,
-                    data: {id:id},
+                    data: {
+                        id: id
+                    },
                 });
-            break;
+                break;
         }
     },
 
     // Support for custom functions, the API is the same as express@4
-    'POST /api/users/create': (req, res) => { res.end('OK'); },
+    'POST /api/users/create': (req, res) => {
+        res.end('OK');
+    },
     // /cnode/api 会被代理到 https://cnodejs.org/api, 不能代理 https
-    'GET /cnode/(.*)':'http://shrek.imdevsh.com'
+    'GET /cnode/(.*)': 'http://shrek.imdevsh.com'
 };
--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 $.ajax({
     url: "/api/users",
-    method:"get",
-    success: function( result ) {
-    }
+    method: "get",
+    success: function (result) {}
 });
 
 $.ajax({
     url: "/api/users/1",
-    method:"get",
-    success: function( result ) {
-    }
+    method: "get",
+    success: function (result) {}
+});
+
+$.ajax({
+    url: "/api/user/133",
+    method: "get",
+    success: function (result) {}
 });
 $.ajax({
     url: "/api/users",
-    method:"post",
-    data:{
-        id:1
+    method: "post",
+    data: {
+        id: 1
     },
-    success: function( result ) {
-    }
+    success: function (result) {}
 });
 $.ajax({
     url: "/api/users/create",
-    method:"post",
+    method: "post",
     data: {
         zipcode: 97201
     },
-    success: function( result ) {
-    }
+    success: function (result) {}
 });
 
 $.ajax({
     url: "/proxy_1/api",
-    method:"get",
-    success: function( result ) {
-    }
+    method: "get",
+    success: function (result) {}
 });
 
 $.ajax({
     url: "/proxy_2/about",
-    method:"get",
-    success: function( result ) {
-    }
+    method: "get",
+    success: function (result) {}
 });
 
 $.ajax({
     url: "/cnode/demo/css3d/js/css3d.js",
-    method:"get",
-    success: function( result ) {
-    }
+    method: "get",
+    success: function (result) {}
 });
